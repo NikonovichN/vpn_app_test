aebdf282090c94542b10eb142afa38edb47ef022
Nikita Nikonovich <nikonovichnd@gmail.com>
2024-11-05 15:35:40 +0300

    add: open_vpn package

--- android/app/src/main/kotlin/com/example/vpn_app_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vpn_app_test/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.vpn_app_test
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import id.laskarmedia.openvpn_flutter.OpenVPNFlutterPlugin
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity () {
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    OpenVPNFlutterPlugin.connectWhileGranted(requestCode == 24 && resultCode == RESULT_OK)
+    super.onActivityResult(requestCode, resultCode, getIntent())
+  }
+}
+  
+
